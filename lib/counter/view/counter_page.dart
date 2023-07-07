3d87a2ccd7b2b80ef96780aa53356bf3a0b5f1f3
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2023-07-08 03:04:19 +0700

    change files

--- lib/counter/view/counter_page.dart
+++ lib/counter/view/counter_page.dart
@@ -29,8 +29,8 @@ class CounterView extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           FloatingActionButton(
-            //onPressed: () => context.read<CounterCubit>().increment(),
-            onPressed: () => throw Exception(),
+            onPressed: () => context.read<CounterCubit>().increment(),
+            // onPressed: () => throw Exception(),
             child: const Icon(Icons.add),
           ),
           const SizedBox(height: 8),
