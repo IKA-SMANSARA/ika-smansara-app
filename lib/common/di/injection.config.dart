3a023740654e4f2e73f944a50b0687c9371cab10
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2023-07-09 13:44:53 +0700

    add network utilities

--- lib/common/di/injection.config.dart
+++ lib/common/di/injection.config.dart
@@ -10,6 +10,7 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:chopper/chopper.dart' as _i3;
+// ignore: depend_on_referenced_packages
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:ika_smansara/common/di/network_module.dart' as _i4;
 import 'package:injectable/injectable.dart' as _i2;
@@ -26,12 +27,14 @@ extension GetItInjectableX on _i1.GetIt {
       environmentFilter,
     );
     final networkModule = _$NetworkModule();
-    gh.factory<String>(
-      () => networkModule.baseURL,
-      instanceName: 'BASEURL',
-    );
-    gh.lazySingleton<_i3.ChopperClient>(
-        () => networkModule.chopperClient(gh<String>(instanceName: 'BASEURL')));
+    gh
+      ..factory<String>(
+        () => networkModule.baseURL,
+        instanceName: 'BASEURL',
+      )
+      ..lazySingleton<_i3.ChopperClient>(
+        () => networkModule.chopperClient(gh<String>(instanceName: 'BASEURL')),
+      );
     return this;
   }
 }
