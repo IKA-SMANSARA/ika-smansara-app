6015775f8664eecc6bc677e5ca41c6ad3bc04fb8
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2023-07-09 11:54:24 +0700

    fix lines_longer_than_80_lines

--- lib/common/utils/extension.dart
+++ lib/common/utils/extension.dart
@@ -1,5 +1,3 @@
-// Chopper already uses the Logger package. Printing the logs to the console requires
-// the following setup.
 import 'package:flutter/material.dart';
 
 void keyboardDismiss(BuildContext context) {
