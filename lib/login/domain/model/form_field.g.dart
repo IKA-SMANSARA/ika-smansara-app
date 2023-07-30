3bcda66d2282142021202ab728e3348290e065da
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2023-07-30 15:06:33 +0700

    slicing login ui done

---
+++ lib/login/domain/model/form_field.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'form_field.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_FormField _$$_FormFieldFromJson(Map<String, dynamic> json) => _$_FormField(
+      EmailField: json['EmailField'] as String? ?? '',
+      PasswordField: json['PasswordField'] as String? ?? '',
+    );
+
+Map<String, dynamic> _$$_FormFieldToJson(_$_FormField instance) =>
+    <String, dynamic>{
+      'EmailField': instance.EmailField,
+      'PasswordField': instance.PasswordField,
+    };
