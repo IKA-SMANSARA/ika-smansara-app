6422dd18da5cc75fea2fa466ae479fe5e80545a8
Muhamad Mashudi Ardi Winata <muhamadmashudiardiwinata@gmail.com>
2023-10-26 14:46:12 +0700

    remove unused import

--- lib/account/presentation/view/screen_size/android/account_portrait_screen.dart
+++ lib/account/presentation/view/screen_size/android/account_portrait_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ika_smansara/account/account.dart';
 
 class AccountPortraitScreen extends StatelessWidget {
