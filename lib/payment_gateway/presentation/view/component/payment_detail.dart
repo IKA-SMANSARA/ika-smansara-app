e51ae8aed75a436019afe8e1accb0984cfcfbde6
Muhamad Mashudi Ardi Winata <muhamadmashudiardiwinata@gmail.com>
2023-11-19 12:54:18 +0700

    remove un used import

--- lib/payment_gateway/presentation/view/component/payment_detail.dart
+++ lib/payment_gateway/presentation/view/component/payment_detail.dart
@@ -2,13 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_form_bloc/flutter_form_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
-import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:ika_smansara/common/presentation/routes/routes.dart';
-import 'package:ika_smansara/common/utils/constants.dart';
 import 'package:ika_smansara/common/utils/extension.dart';
 import 'package:ika_smansara/detail_campaign/detail_campaign.dart';
-import 'package:ika_smansara/payment_gateway/payment_gateway.dart';
 
 class PaymentDetail extends StatelessWidget {
   const PaymentDetail({
