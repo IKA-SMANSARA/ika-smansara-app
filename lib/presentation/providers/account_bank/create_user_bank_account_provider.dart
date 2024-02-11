2047d03d9446315ad6524eedf904c1e43dee01a8
Muhamad Mashudi Ardi Winata <muhamadmashudiardiwinata@gmail.com>
2024-02-11 21:01:57 +0700

    create user bank account provider

---
+++ lib/presentation/providers/account_bank/create_user_bank_account_provider.dart
@@ -0,0 +1,33 @@
+import 'package:ika_smansara/domain/entities/result.dart';
+import 'package:ika_smansara/domain/entities/user_account_bank_document.dart';
+import 'package:ika_smansara/domain/entities/user_account_bank_request.dart';
+import 'package:ika_smansara/domain/usecases/create_account_bank/create_account_bank.dart';
+import 'package:ika_smansara/domain/usecases/create_account_bank/create_account_bank_params.dart';
+import 'package:ika_smansara/presentation/providers/usecase/create_account_bank_use_case_provider.dart';
+import 'package:ika_smansara/utils/constants.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'create_user_bank_account_provider.g.dart';
+
+@riverpod
+Future<UserAccountBankDocument?> createUserBankAccount(
+  CreateUserBankAccountRef ref, {
+  required UserAccountBankRequest userAccountBankRequest,
+}) async {
+  CreateAccountBank createAccountBank = ref.read(
+    createAccountBankUseCaseProvider,
+  );
+
+  var result = await createAccountBank(
+    CreateAccountBankParams(
+      userAccountBankRequest: userAccountBankRequest,
+    ),
+  );
+
+  Constants.logger.d(result.resultValue);
+
+  return switch (result) {
+    Success(value: final campaign) => campaign,
+    Failed(message: _) => null,
+  };
+}
---
+++ lib/presentation/providers/usecase/create_account_bank_use_case_provider.dart
@@ -0,0 +1,14 @@
+import 'package:ika_smansara/domain/usecases/create_account_bank/create_account_bank.dart';
+import 'package:ika_smansara/presentation/providers/repositories/user_bank_account_repository/user_bank_account_repository_provider.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'create_account_bank_use_case_provider.g.dart';
+
+@riverpod
+CreateAccountBank createAccountBankUseCase(CreateAccountBankUseCaseRef ref) {
+  return CreateAccountBank(
+    userBankAccountRepository: ref.watch(
+      userBankAccountRepositoryProvider,
+    ),
+  );
+}
