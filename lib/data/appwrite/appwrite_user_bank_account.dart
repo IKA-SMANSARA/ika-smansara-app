c8f3f2ccdaafc42d6e504d33f6b6d29e8e998f3e
Muhamad Mashudi Ardi Winata <muhamadmashudiardiwinata@gmail.com>
2024-03-23 23:28:51 +0700

    fliter show list bank

--- lib/data/appwrite/appwrite_user_bank_account.dart
+++ lib/data/appwrite/appwrite_user_bank_account.dart
@@ -94,6 +94,7 @@ class AppwriteUserBankAccount implements UserBankAccountRepository {
         collectionId: dotenv.env['ACCOUNT_BANK_DOCUMENT_ID'].toString(),
         queries: [
           Query.equal('isDeleted', false),
+          Query.equal('userId', userId),
         ],
       );
 
