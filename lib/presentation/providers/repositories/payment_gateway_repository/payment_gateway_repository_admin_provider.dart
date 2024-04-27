a64bc31d41efe41353a8e775ff74052632c2b68f
Muhamad Mashudi Ardi Winata <muhamadmashudiardiwinata@gmail.com>
2024-04-27 15:19:27 +0700

    remove unused import

--- lib/presentation/providers/repositories/payment_gateway_repository/payment_gateway_repository_admin_provider.dart
+++ lib/presentation/providers/repositories/payment_gateway_repository/payment_gateway_repository_admin_provider.dart
@@ -1,5 +1,4 @@
 import 'package:ika_smansara/data/payment_gateway/payment_gateway_repository_admin_impl.dart';
-import 'package:ika_smansara/data/payment_gateway/payment_gateway_repository_impl.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'payment_gateway_repository_admin_provider.g.dart';
