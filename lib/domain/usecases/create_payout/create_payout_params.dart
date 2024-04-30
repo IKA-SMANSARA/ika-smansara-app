5f5f37a042eb5cd87a5ad55738ea29116cc46658
Muhamad Mashudi Ardi Winata <muhamadmashudiardiwinata@gmail.com>
2024-05-01 06:50:40 +0700

    remove unused import

--- lib/domain/usecases/create_payout/create_payout_params.dart
+++ lib/domain/usecases/create_payout/create_payout_params.dart
@@ -1,5 +1,4 @@
 import 'package:ika_smansara/domain/entities/payout_item_request.dart';
-import 'package:ika_smansara/domain/entities/payout_request.dart';
 
 class CreatePayoutParams {
   final PayoutItemRequest payoutItemRequest;
