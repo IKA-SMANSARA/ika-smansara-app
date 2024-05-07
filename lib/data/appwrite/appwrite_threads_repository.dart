eae3c4f3dff6021167430b7749bbe40bc7edd2cf
Muhamad Mashudi Ardi Winata <muhamadmashudiardiwinata@gmail.com>
2024-05-07 20:21:00 +0700

    data layer done

--- lib/data/appwrite/appwrite_threads_repository.dart
+++ lib/data/appwrite/appwrite_threads_repository.dart
@@ -1,9 +1,11 @@
 import 'package:appwrite/appwrite.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:ika_smansara/data/helpers/network_client_helper.dart';
 import 'package:ika_smansara/data/repositories/threads_repository.dart';
 import 'package:ika_smansara/domain/entities/result.dart';
 import 'package:ika_smansara/domain/entities/threads_document.dart';
 import 'package:ika_smansara/domain/entities/threads_request.dart';
+import 'package:ika_smansara/utils/constants.dart';
 
 class AppwriteThreadsRepository implements ThreadsRepository {
   final Client _appwriteClient;
@@ -18,45 +20,160 @@ class AppwriteThreadsRepository implements ThreadsRepository {
   Future<Result<ThreadsDocument>> deleteThread({
     required ThreadsRequest threadsRequest,
   }) async {
-    // TODO: implement deleteThread
-    throw UnimplementedError();
+    try {
+      var result = await _databases.updateDocument(
+        databaseId: dotenv.env['DATABASE_ID'].toString(),
+        collectionId: dotenv.env['THREADS_DOCUMENT_ID'].toString(),
+        documentId: threadsRequest.id ?? '',
+        data: threadsRequest.toJson(),
+      );
+
+      Constants.logger.d(result.data);
+
+      return Result.success(
+        ThreadsDocument.fromJson(
+          result.toMap(),
+        ),
+      );
+    } on AppwriteException catch (e) {
+      return Result.failed(e.message ?? 'Error!');
+    }
   }
 
   @override
   Future<Result<ThreadsDocument>> editThread({
     required ThreadsRequest threadsRequest,
   }) async {
-    // TODO: implement editThread
-    throw UnimplementedError();
+    try {
+      var result = await _databases.updateDocument(
+        databaseId: dotenv.env['DATABASE_ID'].toString(),
+        collectionId: dotenv.env['THREADS_DOCUMENT_ID'].toString(),
+        documentId: threadsRequest.id ?? '',
+        data: threadsRequest.toJson(),
+      );
+
+      Constants.logger.d(result.data);
+
+      return Result.success(
+        ThreadsDocument.fromJson(
+          result.toMap(),
+        ),
+      );
+    } on AppwriteException catch (e) {
+      return Result.failed(e.message ?? 'Error!');
+    }
   }
 
   @override
   Future<Result<List<ThreadsDocument>>> getListThreads() async {
-    // TODO: implement getListThreads
-    throw UnimplementedError();
+    try {
+      var result = await _databases.listDocuments(
+        databaseId: dotenv.env['DATABASE_ID'].toString(),
+        collectionId: dotenv.env['THREADS_DOCUMENT_ID'].toString(),
+        queries: [
+          Query.equal('isDeleted', false),
+        ],
+      );
+
+      Constants.logger.d(result.documents);
+
+      if (result.documents.isNotEmpty) {
+        return Result.success(
+          result.documents
+              .map(
+                (e) => ThreadsDocument.fromJson(e.data),
+              )
+              .toList(),
+        );
+      } else {
+        return Result.success([]);
+      }
+    } on AppwriteException catch (e) {
+      return Result.failed(e.message ?? 'Error!');
+    }
   }
 
   @override
   Future<Result<List<ThreadsDocument>>> getListThreadsByUserId({
     required String userId,
   }) async {
-    // TODO: implement getListThreadsByUserId
-    throw UnimplementedError();
+    try {
+      var result = await _databases.listDocuments(
+        databaseId: dotenv.env['DATABASE_ID'].toString(),
+        collectionId: dotenv.env['THREADS_DOCUMENT_ID'].toString(),
+        queries: [
+          Query.equal('isDeleted', false),
+          Query.equal('createdBy', '$userId')
+        ],
+      );
+
+      Constants.logger.d(result.documents);
+
+      if (result.documents.isNotEmpty) {
+        return Result.success(
+          result.documents
+              .map(
+                (e) => ThreadsDocument.fromJson(e.data),
+              )
+              .toList(),
+        );
+      } else {
+        return Result.success([]);
+      }
+    } on AppwriteException catch (e) {
+      return Result.failed(e.message ?? 'Error!');
+    }
   }
 
   @override
   Future<Result<ThreadsDocument>> getThread({
     required String threadId,
   }) async {
-    // TODO: implement getThread
-    throw UnimplementedError();
+    try {
+      var result = await _databases.getDocument(
+        databaseId: dotenv.env['DATABASE_ID'].toString(),
+        collectionId: dotenv.env['THREADS_DOCUMENT_ID'].toString(),
+        documentId: threadId,
+      );
+
+      Constants.logger.d(result.data);
+
+      return Result.success(
+        ThreadsDocument.fromJson(
+          result.toMap(),
+        ),
+      );
+    } on AppwriteException catch (e) {
+      return Result.failed(e.message ?? 'Error!');
+    }
   }
 
   @override
   Future<Result<ThreadsDocument>> postThread({
     required ThreadsRequest threadsRequest,
   }) async {
-    // TODO: implement postThread
-    throw UnimplementedError();
+    try {
+      var result = await _databases.createDocument(
+        databaseId: dotenv.env['DATABASE_ID'].toString(),
+        collectionId: dotenv.env['THREADS_DOCUMENT_ID'].toString(),
+        documentId: ID.unique(),
+        data: threadsRequest.toJson(),
+        permissions: [
+          Permission.write(Role.any()),
+          Permission.read(Role.any()),
+          Permission.update(Role.any()),
+        ],
+      );
+
+      Constants.logger.d(result.data);
+
+      return Result.success(
+        ThreadsDocument.fromJson(
+          result.toMap(),
+        ),
+      );
+    } on AppwriteException catch (e) {
+      return Result.failed(e.message ?? 'Error!');
+    }
   }
 }
--- lib/presentation/pages/contact_us_page/contact_us_page.dart
+++ lib/presentation/pages/contact_us_page/contact_us_page.dart
@@ -1,6 +1,5 @@
 import 'package:adaptive_responsive_util/adaptive_responsive_util.dart';
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ika_smansara/presentation/extensions/async_value_extension.dart';
