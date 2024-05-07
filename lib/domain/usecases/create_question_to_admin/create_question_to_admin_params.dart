6e8499286518ce112eb2914569327515549e677d
Muhamad Mashudi Ardi Winata <muhamadmashudiardiwinata@gmail.com>
2024-05-07 20:44:50 +0700

    create question contact us usecase

---
+++ lib/domain/usecases/create_question_to_admin/create_question_to_admin_params.dart
@@ -0,0 +1,7 @@
+import 'package:ika_smansara/domain/entities/threads_request.dart';
+
+class CreateQuestionToAdminParams {
+  final ThreadsRequest threadsRequest;
+
+  CreateQuestionToAdminParams({required this.threadsRequest});
+}
---
+++ lib/presentation/providers/contact_us/create_question_provider.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/foundation.dart';
+import 'package:ika_smansara/domain/entities/result.dart';
+import 'package:ika_smansara/domain/entities/threads_document.dart';
+import 'package:ika_smansara/domain/entities/threads_request.dart';
+import 'package:ika_smansara/domain/usecases/create_question_to_admin/create_question_to_admin.dart';
+import 'package:ika_smansara/domain/usecases/create_question_to_admin/create_question_to_admin_params.dart';
+import 'package:ika_smansara/presentation/providers/router/router_provider.dart';
+import 'package:ika_smansara/presentation/providers/usecase/create_question_to_admin_use_case_provider.dart';
+import 'package:ika_smansara/utils/constants.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'create_question_provider.g.dart';
+
+@riverpod
+class CreateQuestion extends _$CreateQuestion {
+  @override
+  Future<ThreadsDocument?> build() async => null;
+
+  Future<void> postQuestion({
+    required ThreadsRequest threadsRequest,
+  }) async {
+    state = AsyncValue.loading();
+
+    CreateQuestionToAdmin createQuestionToAdmin = ref.read(
+      createQuestionToAdminUseCaseProvider,
+    );
+
+    var result = await createQuestionToAdmin(
+      CreateQuestionToAdminParams(
+        threadsRequest: threadsRequest,
+      ),
+    );
+
+    Constants.logger.d(result.resultValue);
+
+    switch (result) {
+      case Success(value: final data):
+        state = AsyncData(data);
+        ref.read(routerProvider).pop();
+      case Failed(:final message):
+        state = AsyncError(
+          FlutterError(message),
+          StackTrace.current,
+        );
+        state = const AsyncData(null);
+    }
+  }
+}
