79193913ee117b03b566f1e17629aecd55a3ad49
muhamadmashudiardiwinata <muhamadmashudiardiwinata@gmail.com>
2024-05-20 03:15:39 +0000

    build package

--- lib/presentation/pages/update_account_bank_page/update_account_bank_page.dart
+++ lib/presentation/pages/update_account_bank_page/update_account_bank_page.dart
@@ -11,7 +11,6 @@ import 'package:ika_smansara/presentation/providers/account_bank/update_account_
 import 'package:ika_smansara/presentation/providers/user_data/user_data_provider.dart';
 import 'package:ika_smansara/presentation/widgets/custom_text_button.dart';
 import 'package:ika_smansara/presentation/widgets/custom_text_field.dart';
-import 'package:ika_smansara/utils/constants.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class UpdateAccountBankPage extends ConsumerStatefulWidget {
